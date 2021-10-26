05adc47cf24a638a357723658dcd5519443600ec
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-26 10:35:16 +0700

    add stateless and statefull widget

---
+++ src/components/HelloComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./HelloComponent.css";
+
+// - Stateless Component = component yg dibuat menggunakan functional
+export const HelloComponent = () => {
+  return <p class="text-p">Hello functional component</p>;
+};
---
+++ src/containers/StatefullComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// - Statefull Compent = component yang dibuat menggunakan class
+export class StatefullComponent extends React.Component {
+    render() {
+      return <p>Statefull Component</p>
+    }
+  }
