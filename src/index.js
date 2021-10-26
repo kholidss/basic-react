3c317f522a9c33ab8fa1bd4facb8f69ec1c5629a
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-26 09:45:02 +0700

    basic react hello world

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const HelloWorld = () => {
+  return <p>Hello World!</p>;
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <HelloWorld />,
+  document.getElementById("root")
 );
+
+// atau bisa merender langsung tag HTML
+ReactDOM.render(
+  <p>From React DOM</p>,
+document.getElementById("other")
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
